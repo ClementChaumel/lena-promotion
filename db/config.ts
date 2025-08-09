ad6d68c3c90e6ee4440b276f61004f12a3907305
ClementChaumel <clement.chaumel@gmail.com>
2025-08-09 17:06:02 +0200

    Add db

---
+++ db/config.ts
@@ -0,0 +1,49 @@
+import { column, defineTable, defineDb } from "astro:db";
+
+const Season = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    number: column.number(),
+    year: column.number(),
+  },
+});
+
+const Episode = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    seasonId: column.number({ references: () => Season.columns.id }),
+    number: column.number(),
+    title: column.text(),
+    duration: column.number(),
+  },
+});
+
+const Brand = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    color: column.text(),
+    logo: column.text(),
+  },
+});
+
+const Collab = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    episodeId: column.number({ references: () => Episode.columns.id }),
+    brandId: column.number({ references: () => Brand.columns.id }),
+    description: column.text(),
+    timestamp: column.number(),
+    duration: column.number(),
+    type: column.text(),
+  },
+});
+
+export default defineDb({
+  tables: {
+    Season,
+    Episode,
+    Brand,
+    Collab,
+  },
+});
