f32aba0aa6dea7d550b1957c4b4c1371b6295df7
ClementChaumel <clement.chaumel@gmail.com>
2025-08-12 21:04:42 +0200

    Add FAQ and other pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
+
+import favicons from "astro-favicons";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [
+    db(),
+    favicons({
+      input: {
+        favicons: ["public/favicon.png"],
+      },
+      name: "Lena Promotion",
+    }),
+  ],
 });
